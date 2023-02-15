b490766cb0e6db0e9e96b24f28cd64953bcf1637
Chayan <chayans96@gmail.com>
2023-02-15 13:22:43 +0530

    now preparing for styling through scss

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -19,15 +19,22 @@ module.exports.create = function(req,res){
     });
 }
 
+
+                                     //action to delete the post and comments associated with it 
 module.exports.destroy = function(req,res){
-    Post.findById(req.params.id, function(err,post){// .id means converting object id into string mongoose gives us this featrure)
-        if(post.user == req.user.id)
+                                     //finding post to check if it exists in db or not 
+    Post.findById(req.params.id, function(err,post){
+                                     // .id means converting object id into string mongoose gives us this featrure)
+        if(post.user == req.user.id) // checking if the user deleting the post is the user who has created the post 
+                                     //post.user will return string id and req.user.id will also return string id 
         {
-            post.remove();
+
+            post.remove();           //
             Comment.deleteMany({post: req.params.id},function(err){
                 return res.redirect('back');
             });
-        }else{
+        }else{ 
+                                     //comming into else when post.user != req.user.id
             return res.redirect('back');
         }
     })
--- routes/post.js
+++ routes/post.js
@@ -8,6 +8,6 @@ const postController = require('../controllers/post_controller');
 
 //accessing the actions throgh routes
 router.post('/create', passport.checkAuthentication, postController.create);
-router.get('/destroy/:id',passport.checkAuthentication, postController.destroy);
+router.get('/destroy/:id', passport.checkAuthentication, postController.destroy);
 
 module.exports = router;
